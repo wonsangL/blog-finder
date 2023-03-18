352b74934860134b97122249443d2057c1121b79
wonsangl <harryandws@gmail.com>
2023-03-18 10:18:20 +0900

    PROJECT INIT

---
+++ src/main/java/com/example/blogfinder/BlogFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogfinder/BlogFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
