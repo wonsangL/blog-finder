101a52f30d0cc24ccb2de78e65feb466b5ba99b1
wonsangl <harryandws@gmail.com>
2023-03-19 20:47:10 +0900

    test: blog controller test 외부 응답 의존성 mocking

--- src/test/java/com/example/blogfinder/presentation/blog/BlogControllerTest.java
+++ src/test/java/com/example/blogfinder/presentation/blog/BlogControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.blogfinder.presentation.blog;
 
+import com.example.blogfinder.domain.blog.BlogFinder;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,12 +9,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BlogControllerTest {
     @LocalServerPort
     int port;
+
+    @MockBean
+    private BlogFinder blogFinder;
 
     @BeforeEach
     void setup() {
@@ -23,6 +32,13 @@ class BlogControllerTest {
     @Test
     @DisplayName("키워드를 통해 블로그 검색")
     void findBlogTest() {
+        Blog blog = new Blog("<b>카카오뱅크</b> 비상금대출 조건 연장 이자 어떻게 낮출까?", "", "");
+
+        List<Blog> blogs = Collections.singletonList(blog);
+
+        when(blogFinder.find(any()))
+                .thenReturn(blogs);
+
         RestAssured
                 .given()
                 .queryParam("query", "카카오뱅크")
@@ -34,6 +50,6 @@ class BlogControllerTest {
                 .then()
                 .statusCode(200)
                 .assertThat()
-                .body("data[0].title", is("<b>카카오</b><b>뱅크</b> - 나무위키"));
+                .body("data[0].title", is("<b>카카오뱅크</b> 비상금대출 조건 연장 이자 어떻게 낮출까?"));
     }
 }
