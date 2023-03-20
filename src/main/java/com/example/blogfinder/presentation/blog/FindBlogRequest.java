8ffd839869ee6ea413f1b1bbdc06eb4a79956276
wonsangl <harryandws@gmail.com>
2023-03-20 22:39:41 +0900

    fix: sort default value 설정

--- src/main/java/com/example/blogfinder/presentation/blog/FindBlogRequest.java
+++ src/main/java/com/example/blogfinder/presentation/blog/FindBlogRequest.java
@@ -2,6 +2,7 @@ package com.example.blogfinder.presentation.blog;
 
 public record FindBlogRequest(String query, String sort, Integer page, Integer size) {
     public FindBlogRequest {
+        sort = sort == null ? "accuracy" : sort;
         page = page == null ? 1 : page;
         size = size == null ? 10 : size;
     }
