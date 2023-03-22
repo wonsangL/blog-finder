f3e773245fd1ba4411c194c9cb7a5ac67378b8c7
wonsangl <harryandws@gmail.com>
2023-03-22 22:04:56 +0900

    test: 블로그 검색 실패 테스트 작성

--- src/test/java/com/example/blogfinder/presentation/blog/BlogControllerTest.java
+++ src/test/java/com/example/blogfinder/presentation/blog/BlogControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.blogfinder.presentation.blog;
 
+import com.example.blogfinder.domain.blog.CannotFoundBlogException;
 import com.example.blogfinder.domain.blog.FindBlogResult;
 import com.example.blogfinder.domain.blog.kakao.KakaoBlog;
 import com.example.blogfinder.domain.blog.kakao.KakaoBlogClient;
@@ -9,9 +10,12 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 
 import java.time.OffsetDateTime;
 import java.util.Collections;
@@ -26,12 +30,18 @@ class BlogControllerTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    private CacheManager cacheManager;
+
     @MockBean
     private KakaoBlogClient blogFinder;
 
     @BeforeEach
     void setup() {
         RestAssured.port = port;
+
+        Cache blog = cacheManager.getCache("blog");
+        if(blog != null) blog.clear();
     }
 
     @Test
@@ -58,5 +68,24 @@ class BlogControllerTest {
                 .statusCode(200)
                 .assertThat()
                 .body("data[0].title", is("<b>카카오뱅크</b> 비상금대출 조건 연장 이자 어떻게 낮출까?"));
+    }
+
+    @Test
+    @DisplayName("모든 블로그 검색 소스 에러 발생")
+    void findBlogFailTest() {
+        when(blogFinder.find(any()))
+                .thenThrow(CannotFoundBlogException.class);
+
+        RestAssured
+                .given()
+                .queryParam("query", "카카오뱅크")
+
+                .when()
+                .get("/blog")
+
+                .then()
+                .statusCode(500)
+                .assertThat()
+                .body("message", is("모든 블로그 검색 소스에서 블로그 검색에 실패하였습니다."));
     }
 }
